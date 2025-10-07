d96d18169b5c3a4b27d854b67c0dd40179dc65b4
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-10-07 14:00:52 +0100

    feature(data model): Category entity

---
+++ src/main/java/com/znaji/InventoryManagementSystem/entity/Category.java
@@ -0,0 +1,50 @@
+package com.znaji.InventoryManagementSystem.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return 2001;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof Category c)) {
+            return false;
+        }
+        return this.id != null && this.id.equals(c.id);
+    }
+}
