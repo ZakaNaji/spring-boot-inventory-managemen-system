a4bbf615a82ea8341e8faa0bf548c01b2d2040b5
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-10-07 22:07:23 +0100

    feature(Repositories): UserRepository (find by email )

---
+++ src/main/java/com/znaji/InventoryManagementSystem/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.znaji.InventoryManagementSystem.repository;
+
+import com.znaji.InventoryManagementSystem.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByEmailIgnoreCase(String email);
+}
