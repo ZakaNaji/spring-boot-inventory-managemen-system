c8c1be3a2efd3e0b5812141aa624e6b1488e267a
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-10-07 12:39:26 +0100

    feature(data model): user entity

---
+++ src/main/java/com/znaji/InventoryManagementSystem/entity/User.java
@@ -0,0 +1,33 @@
+package com.znaji.InventoryManagementSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    private String phoneNumber;
+
+    @Enumerated
+    private UserRole role;
+
+    @Column(nullable = false)
+    private final LocalDateTime createdAt = LocalDateTime.now();
+}
