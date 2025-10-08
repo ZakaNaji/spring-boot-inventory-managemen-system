aec0a6e50d855e215dfd4e6030bcdcbea046f500
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-10-08 17:16:50 +0100

    feature(security): custom auth user

---
+++ src/main/java/com/znaji/InventoryManagementSystem/security/AuthUser.java
@@ -0,0 +1,36 @@
+package com.znaji.InventoryManagementSystem.security;
+
+import com.znaji.InventoryManagementSystem.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@RequiredArgsConstructor
+public class AuthUser implements UserDetails {
+
+    private final User user;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
