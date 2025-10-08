674ca61ca896533d5112bf37af051a03569bd6e9
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-10-08 15:18:18 +0100

    feature(exception handling): global controller advice ta handle diffrent exceptions result

---
+++ src/main/java/com/znaji/InventoryManagementSystem/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.znaji.InventoryManagementSystem.exception;
+
+import com.znaji.InventoryManagementSystem.dto.response.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse globalExceptionHandler(Exception ex, HttpServletRequest request) {
+        return ErrorResponse.of(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse notFound(NotFoundException ex, HttpServletRequest request) {
+        return ErrorResponse.of(
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+    }
+
+}
---
+++ src/main/java/com/znaji/InventoryManagementSystem/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.znaji.InventoryManagementSystem.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
