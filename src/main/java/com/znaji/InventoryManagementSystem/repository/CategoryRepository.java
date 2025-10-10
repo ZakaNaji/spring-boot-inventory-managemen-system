a4bbd5c3e0969b18eeb6bda796146425e81a7eda
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-10-10 18:09:49 +0100

    feature(category repository): Category jpa repository

--- src/main/java/com/znaji/InventoryManagementSystem/repository/CategoryRepository.java
+++ src/main/java/com/znaji/InventoryManagementSystem/repository/CategoryRepository.java
@@ -1,7 +1,16 @@
 package com.znaji.InventoryManagementSystem.repository;
 
+import com.znaji.InventoryManagementSystem.dto.response.CategoryResponse;
 import com.znaji.InventoryManagementSystem.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findByName(String name);
+
+    List<CategoryResponse> findAllBy();
+
+    Optional<CategoryResponse> findCategoryById(Long id);
 }
