72604215651684ed4ac7c5bc65e506bd0b954c31
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-10-10 18:25:50 +0100

    feature(supplier repository): Supplier jpa repo

--- src/main/java/com/znaji/InventoryManagementSystem/repository/SupplierRepository.java
+++ src/main/java/com/znaji/InventoryManagementSystem/repository/SupplierRepository.java
@@ -1,7 +1,14 @@
 package com.znaji.InventoryManagementSystem.repository;
 
+import com.znaji.InventoryManagementSystem.dto.response.SupplierResponse;
 import com.znaji.InventoryManagementSystem.entity.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+    List<SupplierResponse> findAllBy();
+
+    Optional<SupplierResponse> findSupplierById(Long id);
 }
