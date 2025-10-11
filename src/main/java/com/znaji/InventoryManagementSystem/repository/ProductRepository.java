4512216303b65d9cbe027447efc6adfbe435c372
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-10-11 13:40:05 +0100

    feature(product): product jpa repository

--- src/main/java/com/znaji/InventoryManagementSystem/repository/ProductRepository.java
+++ src/main/java/com/znaji/InventoryManagementSystem/repository/ProductRepository.java
@@ -1,12 +1,18 @@
 package com.znaji.InventoryManagementSystem.repository;
 
-import com.znaji.InventoryManagementSystem.dto.response.ProductView;
+import com.znaji.InventoryManagementSystem.dto.response.ProductResponse;
 import com.znaji.InventoryManagementSystem.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-  List<ProductView> findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String name, String description);
+    List<ProductResponse> findAllBy();
 
+    Optional<ProductResponse> findProductById(Long id);
+
+    List<ProductResponse> findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCase(String name, String description);
+
+    Optional<Product> findBySkuIgnoreCase(String sku);
 }
